64a815b36a18aadb0b4e3d6c56e4d7e0a8275307
yusuke968 <2201110070ze@cyber-u.ac.jp>
2024-12-05 04:58:12 +0900

    replace dummy uri

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定 
 const { MongoClient } = require("mongodb");
-const uri = "**********";
+const uri = "mongodb+srv://2201110070ze:<password>@cluster0.smmvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => { 
